15b00b6eec54cfd5aee5e13d858628ba87cc9dd5
khurram jaffery <khurramjaffery12@gmail.com>
2023-09-02 15:03:24 +0500

    first commit

---
+++ code/bootstrap 5 using grid webside blank/style.s
@@ -0,0 +1,47 @@
+education2{
+    width: 100%;
+    height: 650px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    border: 1px solid transparent;
+   
+   
+}
+.education3{
+    width: 100%;
+    height: auto;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    border: 1px solid red;
+
+   
+}
+.education4{
+    width: 100%;
+    height: 600px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    border: 1px solid transparent;
+
+   
+}
+.education5{
+    width: 100%;
+    height: fit-content;
+    padding-top: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    border: 1px solid transparent;
